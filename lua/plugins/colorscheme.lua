78b5040d35b4e33920ec4ddb05eb53a0054adb5e
Victor <victor@educar.se>
2023-12-07 11:19:30 +0100

    Updated themes and go add ins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,20 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+
+    lazy = false,
+
+    priority = 1000,
+
+    opts = {},
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "tokyonight",
     },
   },
 }
